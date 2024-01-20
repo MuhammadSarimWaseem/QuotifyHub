02b829392291f6ac162eb8026d78b639f1ac94ef
muhammadsarimwaseem <sarimwaseem84@gmail.com>
2024-01-20 15:31:10 +0500

    updated

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Quotes from './Quotes/Quotes';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Quotes></Quotes>
+    </div>
+  );
+}
+
+export default App
---
+++ src/Quotes/Quotes.jsx
@@ -0,0 +1,66 @@
+import React, { Fragment, useCallback, useEffect, useState } from 'react';
+import './Quotes.css';
+
+function Quotes() {
+  const [quotes, setQuotes] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchQuotes = useCallback(async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetch('https://dummyjson.com/quotes');
+
+      if (!response.ok) {
+        throw new Error('Failed to fetch quotes');
+      }
+
+      const data = await response.json();
+      const quotesData = data.quotes || [];
+
+      const transformedQuotes = quotesData.map(quoteData => ({
+        text: quoteData.quote,
+        name: quoteData.author
+      }));
+
+      setQuotes(transformedQuotes);
+      setLoading(false);
+
+    } catch (error) {
+      console.error('Error fetching quotes:', error.message);
+      setQuotes([]);
+      setLoading(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchQuotes();
+  }, [fetchQuotes]);
+
+  return (
+    <Fragment>
+      <section className='container'>
+        <h1>QUOTIFYHUB</h1>
+      </section>
+      <section className='button-container'>
+        <button className='fetch-button' onClick={fetchQuotes}>Fetch</button>
+      </section>
+      <section className='quotes-container'>
+        {!loading && quotes.length > 0 ? (
+          <ul>
+            {quotes.map((quote, index) => (
+              <li className='quote' key={index}>
+                <h2>- {quote.name}</h2>
+                <p>" {quote.text} "</p>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p className='loading-message'>{loading ? 'Loading....' : 'Quotes not found'}</p>
+        )}
+      </section>
+    </Fragment>
+  );
+}
+
+export default Quotes;
